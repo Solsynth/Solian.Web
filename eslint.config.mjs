52971c2d672eb435ed76ab775c82d394021d6235
LittleSheep <littlesheep.code@hotmail.com>
2025-09-24 00:58:15 +0800

    :bug: Fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-import tailwind from "eslint-plugin-tailwindcss"
-
 export default withNuxt(
   // Your custom configs here
   {
-    ...tailwind.configs["flat/recommended"],
     rules: {
       "vue/multi-word-component-names": "off",
       "vue/no-v-html": "off",
