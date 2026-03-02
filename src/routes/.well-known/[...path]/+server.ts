4dbf8e63dce3c702ea518b12a565bdb642e52cf9
LittleSheep <littlesheep.code@hotmail.com>
2026-03-03 00:27:44 +0800

    :recycle: Update the proxy logic

---
+++ src/routes/.well-known/[...path]/+server.ts
@@ -0,0 +1,45 @@
+import type { RequestHandler } from './$types';
+
+const API_BASE = 'api.solian.app';
+
+export const GET: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const POST: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const PUT: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const PATCH: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const DELETE: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const HEAD: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
+
+export const OPTIONS: RequestHandler = async ({ request }) => {
+	const url = new URL(request.url);
+	url.hostname = API_BASE;
+	return fetch(url.toString(), request);
+};
